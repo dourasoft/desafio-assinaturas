4e7df4d945c0f7706b23db1f3fe850ada931c54b
Marcos Lourenço <marcosrealbmata@gmail.com>
2024-05-03 09:42:40 -0300

    migration base files

---
+++ database/migrations/2024_05_03_122430_create_assinaturas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('signature', function (Blueprint $table) {
+            $table->id();
+            $table->string('cadastro')->unique();
+            $table->string('descricao');
+            $table->string('valor');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('signature');
+    }
+};
