e864243814a7232623e73cfc13e5543110d727ab
Marcos Lourenço <marcosrealbmata@gmail.com>
2024-05-04 19:47:31 -0300

    add a Schedule to run a command daily
    - php artisan schedule:work  (para iniciar a execução da tarefa programada )
    
    - php artisan schedule:list (para ver as tarefas a serem executadas)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
+
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('app:verificar-assinaturas')->daily();
+    })
+
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
